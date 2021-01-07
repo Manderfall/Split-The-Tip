d7b0f6ab5bd18c9e033ea07c5c95bcb4c415b442
Manderfall <Manderfall@gmail.com>
2021-01-06 20:42:11 -0500

    setup calc logic

---
+++ public/js/calcLogic.js
@@ -0,0 +1,23 @@
+
+ document.querySelector('#tip-form').onchange = function(){
+
+  const bill = Number(document.getElementById('billTotal').value);
+  const tip = Number(document.getElementById('tipInput').value);  
+  const people = Number(document.getElementById('tippersPresent').value);
+  const tipValue = bill * (tip/100)
+  const finalBill = bill + tipValue
+  const splitBill = finalBill/people
+console.log(finalBill.toFixed(2))
+const tipAmount = document.querySelector('#tipAmount')
+const totalBillWithTip = document.querySelector('#totalBillWithTip')
+const tipPerPerson = document.querySelector('#tipPerPerson')
+tipAmount.value = tipValue.toFixed(2);
+ totalBillWithTip.value = finalBill.toFixed(2);
+ tipPerPerson.value = splitBill.toFixed(2);
+
+
+
+ //Show Results
+
+  document.getElementById('results').style.display='block'
+}
